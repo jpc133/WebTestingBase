318f6c5946c69d1bcd9e8fadcbef406bbfd62ff0
jpc133 <jonathan.clarke73@gmail.com>
2017-04-25 12:43:47 +0100

    Added changes from other projects which affected FileHandling, this adds some better error handling and a method which writes .CSVs

--- CoypuTestingSetup/FileHandling.cs
+++ CoypuTestingSetup/FileHandling.cs
@@ -8,7 +8,15 @@ namespace CoypuTestingSetup
     {
         public static int[] getCSVLength(string path)
         {
-            FileStream fs = File.OpenRead(path);
+            FileStream fs;
+            try
+            {
+                fs = File.OpenRead(path);
+            }
+            catch
+            {
+                throw new Exception("File is either open or doesn't exist");
+            }
             StreamReader sr = new StreamReader(fs);
             int y = 0;
             int x = 0;
@@ -18,7 +26,7 @@ namespace CoypuTestingSetup
                 x = str.Split(',').Length;
                 y++;
             }
-            return new int[2] {x,y};
+            return new int[2] { x, y };
         }
 
         public static string[,] readCSV(string path)
@@ -26,8 +34,15 @@ namespace CoypuTestingSetup
             String[,] spreadArray;
             int[] lengths = getCSVLength(path);
             spreadArray = new string[lengths[0], lengths[1]];
-
-            FileStream fs = File.OpenRead(path);
+            FileStream fs;
+            try
+            {
+                fs = File.OpenRead(path);
+            }
+            catch
+            {
+                throw new Exception("File is either open or doesn't exist");
+            }
             StreamReader sr = new StreamReader(fs);
             CsvReader csvReader = new CsvReader(sr);
 
@@ -49,8 +64,34 @@ namespace CoypuTestingSetup
                 }
                 lineNum++;
             }
-
+            fs.Close();
             return spreadArray;
+        }
+
+        public static void writeCSV(string[,] data, string path)
+        {
+            FileStream fs = File.OpenWrite(path);
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                for (int y = 0; y < data.GetLength(1); y++)
+                {
+                    string s = "";
+                    for (int x = 0; x < data.GetLength(0); x++)
+                    {
+                        if (x < data.GetLength(0) - 1)
+                        {
+                            s += data[x, y] + ",";
+                        }
+                        else
+                        {
+                            s += data[x, y];
+                        }
+                    }
+                    sw.WriteLine(s, System.Text.Encoding.UTF8);
+                }
+                sw.Flush();
+            }
+            fs.Close();
         }
     }
 }
