ef8dfcb45fa9f960a903b6094c357d3bcdb878da
jpc133 <jonathan.clarke73@gmail.com>
2017-05-02 13:37:05 +0100

    Split into a class library and a unit test setup.

---
+++ CoypuTestingSetup/WebTesting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coypu;
+using CoypuTestingSetup;
+
+namespace TestExample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SelfTest()
+        {
+            BrowserSession browser = CoypuHelper.setupTestEnviroment(true,"https://www.google.com");
+            string[,] data = FileHandling.readCSV("C:\\Users\\jonat\\Documents\\dataFile.csv");
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                browser.Visit("https://www.bing.com");
+                browser.FindId("sb_form_q").SendKeys(data[0,i]);
+                browser.FindId("sb_form_go").Click();
+            }
+            browser.Dispose();
+        }
+
+
+        [TestMethod]
+        public void Test2()
+        {
+            //                                                    headless, URL
+            BrowserSession browser = CoypuHelper.setupTestEnviroment(false, "https://www.google.com");
+            browser.Visit("https://www.google.com");
+
+            browser.Dispose();
+        }
+
+    }
+}
---
+++ CoypuWebTestingBase/HeadlessProfile.cs
@@ -0,0 +1,54 @@
+﻿using Coypu.Drivers;
+using Coypu.Drivers.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace CoypuTestingSetup
+{
+    //In the following two classes replace 172.20.1.114:4444 with the address and port of your selenium server
+    class HeadlessChromeWebdriver : SeleniumWebDriver
+    {
+        public HeadlessChromeWebdriver(Browser browser)
+            : base(new RemoteWebDriver(new Uri(@"http://172.20.1.114:4444/wd/hub"), CapabilitiesNew.HeadlessChrome()), browser)
+        {
+        }
+    }
+
+    class ChromeWebdriver : SeleniumWebDriver
+    {
+        public ChromeWebdriver(Browser browser)
+            : base(new RemoteWebDriver(new Uri(@"http://172.20.1.114:4444/wd/hub"), CapabilitiesNew.Chrome()), browser)
+        {
+        }
+    }
+
+
+    static class CapabilitiesNew{
+        static public DesiredCapabilities HeadlessChrome() 
+        {
+            ChromeOptions co = new ChromeOptions();
+            co.AddArgument("--headless");
+            co.AddArgument("--disable-gpu");
+            co.AddArgument("--no-sandbox");
+            //Setting a remote debugging port causes issues
+
+            DesiredCapabilities dc = DesiredCapabilities.Chrome();
+            dc.SetCapability(ChromeOptions.Capability, co);
+
+            return (DesiredCapabilities)co.ToCapabilities();
+        }
+
+        static public DesiredCapabilities Chrome()
+        {
+            ChromeOptions co = new ChromeOptions();
+            co.AddArgument("--no-sandbox");
+            //Setting a remote debugging port causes issues
+
+            DesiredCapabilities dc = DesiredCapabilities.Chrome();
+            dc.SetCapability(ChromeOptions.Capability, co);
+
+            return (DesiredCapabilities)co.ToCapabilities();
+        }
+    }
+}
